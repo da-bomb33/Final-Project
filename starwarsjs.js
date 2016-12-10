adc0720e3d3f3b25f46531e78f3193a65dc22fc5
da-bomb33 <wincrew2011@gmail.com>
2016-12-10 12:59:23 -0600

    Add files via upload

---
+++ starwarsjs.js
@@ -0,0 +1,58 @@
+function speciesFunction()
+{
+	var M;
+		M = document.getElementById("M").value;
+	switch(M){
+		case "january":
+			M = "Wookie's";
+			document.getElementById("month").innerHTML = "Your species are the " + M;
+			break;	
+		case "febuary":
+			M = "Neimoidian's";
+			document.getElementById("month").innerHTML = "Your species are the " + M;
+			break;
+		case "march":
+			M = "Zabarak's";
+			document.getElementById("month").innerHTML = "Your species are the " + M;
+			break;
+		case "april":
+			M = "Sith";
+			document.getElementById("month").innerHTML = "Your species are the " + M;
+			break;	
+		case "may":
+			M = "Sand People";
+			document.getElementById("month").innerHTML = "Your species are the " + M;
+			break;
+		case "june":
+			M = "Jawa";
+			document.getElementById("month").innerHTML = "Your species are the " + M;
+			break;
+		case "july":
+			M = "Rodian";
+			document.getElementById("month").innerHTML = "Your species are the " + M;
+			break;
+		case "august":
+			M = "Yuuzhan Vong";
+			document.getElementById("month").innerHTML = "Your species are the " + M;
+			break;
+		case "september":
+			M = "Hutt";
+			document.getElementById("month").innerHTML = "Your species are the " + M;
+			break;
+		case "october":
+			M = "Besalisk";
+			document.getElementById("month").innerHTML = "Your species are the " + M;
+			break;
+		case "november":
+			M = "Bith";
+			document.getElementById("month").innerHTML = "Your species are the " + M;
+			break;
+		case "december":
+			M = "Dug";
+			document.getElementById("month").innerHTML = "Your species are the " + M;
+			break;
+		}
+	
+			
+}
+
